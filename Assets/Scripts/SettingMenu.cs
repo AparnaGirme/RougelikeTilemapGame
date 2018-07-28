88161312dc3ef6fcabc54790122271fd632fc340
Aparna Girme <agirme54@gmail.com>
2018-07-28 13:20:40 -0700

    first commit

---
+++ Assets/Scripts/SettingMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingMenu : MonoBehaviour
+{
+
+    public AudioMixer audioMixer;
+    public void SetVolume(float volume)
+    {
+        //Debug.Log("Volume changed to " + volume);
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+}
